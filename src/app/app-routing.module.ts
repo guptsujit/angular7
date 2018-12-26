f022112156c77e81dc1217e4ac9e23c1eacf879f
Sujit Kumar <sujit.kumar@irissoftware.com>
2018-12-26 10:11:21 +0530

    created custom preloading strategy service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule,PreloadAllModules } from '@angular/router';
 import {HomeComponent} from './home/home.component';
+import {CustomPreloadingService} from './custom-preloading.service';
 const routes: Routes = [
  { path: '', redirectTo: '/home', pathMatch: 'full' },
  { path: 'home', component: HomeComponent },
- { path: 'employees', loadChildren: './employee/employee.module#EmployeeModule' },
+ { path: 'employees',data : {preload :true},loadChildren: './employee/employee.module#EmployeeModule' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes,{preloadingStrategy:PreloadAllModules})],
+  imports: [RouterModule.forRoot(routes,{preloadingStrategy:CustomPreloadingService})],
   exports : [RouterModule]
 })
 export class AppRoutingModule { }
